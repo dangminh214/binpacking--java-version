6f38b5491a671a7f32fdff8bd28c59881b6fa944
Dang Minh Nguyen <dangminh214@gmail.com>
2026-01-29 00:21:30 +0100

    temp add local search

---
+++ src/model/algorithm/localsearch/LocalSearchAlgorithm.java
@@ -0,0 +1,16 @@
+package model.algorithm.localsearch;
+
+import java.util.ArrayList;
+import model.algorithm.AbstractSolution;
+import model.core.Item;
+
+import model.core.Item;
+
+public class LocalSearchAlgorithm<I extends Item, C, S extends AbstractSolution<C>> {
+    private int numBox = 0;
+    private double occupiedRate = 0.0;
+
+    public LocalSearchAlgorithm() {
+        S initialSolution;
+    }
+}
